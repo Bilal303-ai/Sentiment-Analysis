36e413a7c42f48545ec84aea8fc9866034527912
Mohd Bilal Hasan <bilalhasan403@gmail.com>
2024-11-03 14:22:57 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,10 +1,23 @@
+import tensorflow as tf
 from tensorflow.keras.models import load_model
+import json
 
-model = load_model("Sentiments classifier.keras")
+fnet_classifier = load_model("Sentiments classifier.keras")
 
-review_example = ''
+review_example = input("Input your review")
+
+with open("vocab.json", "r") as f:
+    vocab = json.load(f)
+
+seq_max_length = 512
+tokenizer = keras_nlp.tokenizers.WordPieceTokenizer(
+    vocabulary=vocab,
+    lowercase=False,
+    sequence_length=seq_max_length,
+)
+
 def make_prediction(sentence):
-    tokens = tokenizer(sentence)
+    tokens = tokenizer(review_example)
     tokens = tf.expand_dims(tokens, 0)
     prediction = fnet_classifier.predict(tokens, verbose=0)
 
